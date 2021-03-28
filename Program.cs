c188271e3dfe2aaa8429d03cd0e8295fc584d0a6
andyT994 <80651103+andyT994@users.noreply.github.com>
2021-03-28 19:47:20 +1100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace SelectionExercise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter User Name");
+            string Username = Console.ReadLine();
+            
+            // Selection - if
+            if (Username == "AndyT") {
+            System.Console.WriteLine("Username Exist");
+
+             Console.WriteLine("Enter Your Password");
+            string Pass = Console.ReadLine();
+
+            if (Pass == "Acdb") {
+                System.Console.WriteLine("Login Successful");
+            } else {
+                System.Console.WriteLine("Login Unsuccessful");
+
+            }    
+
+        } else {
+            System.Console.WriteLine("Login Unsuccessful");
+      }
+    }
+  }
+} 
